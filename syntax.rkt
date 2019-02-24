57bfc4099ee2a174b46e52e00405ae0a1d03dd89
helado de brownie <helado.de.brownie@gmail.com>
2019-02-24 17:44:33 -0500

    allow combiners other than ~a and provide $> which uses begin as combiner

--- syntax.rkt
+++ syntax.rkt
@@ -4,7 +4,8 @@
   "struct.rkt")
 (provide
   define-rule
-  ~>)
+  ~>
+  $>)
 
 (define-syntax define-rule
   (syntax-parser
@@ -19,17 +20,22 @@
   (syntax-parser
     ((_ expression:expr ...
         (~optional (~seq #:weight weight:exact-positive-integer)
-                   #:defaults ((weight #'1))))
-     #'(clause (λ () (~a expression ...))
+                   #:defaults ((weight #'1)))
+        (~optional (~seq #:combiner combiner:expr)
+                   #:defaults ((combiner #'~a))))
+     #'(clause (λ () (combiner expression ...))
                weight))))
+
+(define-syntax-rule ($> form ...)
+  (~> form ... #:combiner begin))
 
 (module+ main
   (define-rule start
     (~> (greeting) " :3"))
 
   (define-rule greeting
-    (~> "hewwo" #:weight 9)
-    (~> "hoi")
+    ($> "hewwo" #:weight 9)
+    ($> "hoi")
     (~> "this clause always backtracks" (empty) #:weight 10000))
 
   (define-rule empty)
