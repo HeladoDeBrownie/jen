704a7586b864ee0bbf61ccbc75c8b3c13d997d05
helado de brownie <helado.de.brownie@gmail.com>
2019-02-24 06:05:35 -0500

    add equivalent samples to syntax

--- syntax.rkt
+++ syntax.rkt
@@ -3,3 +3,17 @@
 
 (define-syntax (define-rule a-syntax)
   (raise-syntax-error #f "TODO" a-syntax))
+
+(module+ main
+  (define-rule start
+    (~> (greeting) " :3"))
+
+  (define-rule greeting
+    (~> "hewwo" #:weight 9)
+    (~> "hoi")
+    (~> "this clause always backtracks" (empty)))
+
+  (define-rule empty)
+
+  (for ((_ (in-range 100)))
+    (displayln (start))))
