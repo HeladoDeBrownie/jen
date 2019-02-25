a932c82e89ffbbcc795af0633c34c8184aec7aa6
helado de brownie <helado.de.brownie@gmail.com>
2019-02-24 20:43:18 -0500

    add once, which implicitly uses a flag to ensure a clause only happens once

--- state.rkt
+++ state.rkt
@@ -1,12 +1,16 @@
 #lang racket
-(require "semantics.rkt")
-(provide (contract-out
-          (turn-on (any/c . -> . void))
-          (turn-off (any/c . -> . void))
-          (on? (any/c . -> . boolean?))
-          (off? (any/c . -> . boolean?))
-          (needs (any/c . -> . void))
-          (needs-not (any/c . -> . void))))
+(require
+  (for-syntax syntax/parse)
+  "semantics.rkt")
+(provide
+  (contract-out
+   (turn-on (any/c . -> . void))
+   (turn-off (any/c . -> . void))
+   (on? (any/c . -> . boolean?))
+   (off? (any/c . -> . boolean?))
+   (needs (any/c . -> . void))
+   (needs-not (any/c . -> . void)))
+  once)
 
 (define (turn-on a-flag)
   (rule-state-flags (set-add (rule-state-flags) a-flag)))
@@ -27,3 +31,11 @@
 (define (needs-not a-flag)
   (unless (off? a-flag)
     (backtrack)))
+
+(define-syntax (once a-syntax)
+  (define a-flag (gensym))
+  (syntax-parse a-syntax
+    ((_)
+     #`(begin
+         (needs-not '#,a-flag)
+         (turn-on '#,a-flag)))))
