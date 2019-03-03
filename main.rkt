f2cd98160d38bde3bddcef486409760c40cad111
helado de brownie <helado.de.brownie@gmail.com>
2019-03-03 05:07:11 -0500

    unname evaluate-rule; give that name to a procedure that catches backtrack

--- main.rkt
+++ main.rkt
@@ -1,7 +1,9 @@
 #lang racket
 (require
   "syntax.rkt"
-  "state.rkt")
+  "state.rkt"
+  (only-in "semantics.rkt" evaluate-rule))
 (provide (all-from-out
           "syntax.rkt"
-          "state.rkt"))
+          "state.rkt"
+          "semantics.rkt"))
--- semantics.rkt
+++ semantics.rkt
@@ -3,21 +3,28 @@
   "private/weighted-set.rkt")
 (provide
  (contract-out
+  (evaluate-rule ((rule?) (#:default any/c) . ->* . any/c))
   (struct rule
     ((clauses (hash/c (-> any/c) (-> exact-nonnegative-integer?)))))
+  (backtrack (-> none/c))
   (struct exn:backtrack
     ((message string?)
-     (continuation-marks continuation-mark-set?)))
-  (backtrack (-> none/c))))
+     (continuation-marks continuation-mark-set?)))))
 
-(define (evaluate-rule a-rule)
-  (let loop ((untried-clauses (rule->weighted-set a-rule)))
-    (when (weighted-set-empty? untried-clauses)
-      (backtrack))
-    (define-values (clause-to-try untried-clauses_)
-      (weighted-set-remove-random untried-clauses))
-    (with-handlers ((exn:backtrack? (λ (_) (loop untried-clauses_))))
-      (clause-to-try))))
+(define (evaluate-rule a-rule #:default (default-value #f))
+  (with-handlers ((exn:backtrack? (λ (_) default-value)))
+    (a-rule)))
+
+(struct rule (clauses)
+  #:property prop:procedure
+  (λ (this-rule)
+    (let loop ((untried-clauses (rule->weighted-set this-rule)))
+      (when (weighted-set-empty? untried-clauses)
+        (backtrack))
+      (define-values (clause-to-try untried-clauses_)
+        (weighted-set-remove-random untried-clauses))
+      (with-handlers ((exn:backtrack? (λ (_) (loop untried-clauses_))))
+        (clause-to-try)))))
 
 (define (rule->weighted-set a-rule)
   (weighted-set
@@ -28,6 +35,3 @@
   (raise (exn:backtrack "backtrack" (current-continuation-marks))))
 
 (struct exn:backtrack exn ())
-
-(struct rule (clauses)
-  #:property prop:procedure evaluate-rule)
