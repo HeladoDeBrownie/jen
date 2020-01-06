566f37f97c59060985b431383ffaa5aa8ea0c47b
helado de brownie <helado.de.brownie@gmail.com>
2020-01-06 17:02:46 -0500

    add cycle/repeat-last

--- jen-lib/sequential.rkt
+++ jen-lib/sequential.rkt
@@ -3,7 +3,8 @@
   syntax/parse/define
   "base.rkt")
 (provide
-  cycle)
+  cycle
+  cycle/repeat-last)
 
 (define-syntax-parser cycle
   ((_ e:expr ...+)
@@ -15,3 +16,14 @@
          ((list-ref #,thunks (#,current-count)))
          (#,current-count
           (modulo (add1 (#,current-count)) (length #,thunks)))))))
+
+(define-syntax-parser cycle/repeat-last
+  ((_ e:expr ...+)
+   (let ((current-count
+          (syntax-local-lift-expression #'(make-rule-parameter 0)))
+         (thunks
+          (syntax-local-lift-expression #'(list (thunk e) ...))))
+     #`(begin0
+         ((list-ref #,thunks (#,current-count)))
+         (#,current-count
+          (min (add1 (#,current-count)) (sub1 (length #,thunks))))))))
